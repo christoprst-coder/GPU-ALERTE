9c1f61cf6ca7ac30b9a72d3f868c2dbfd1e74837
christoprst-coder <christo.prst@gmail.com>
2026-06-20 13:08:51 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import discord
 import os
+from discord.ext import tasks
 
 TOKEN = os.getenv("DISCORD_TOKEN")
 CHANNEL_ID = 1517834960751427668
@@ -10,10 +11,15 @@ client = discord.Client(intents=intents)
 @client.event
 async def on_ready():
     print(f"Connecté en tant que {client.user}")
+    check_prices.start()
 
+@tasks.loop(minutes=15)
+async def check_prices():
     channel = client.get_channel(CHANNEL_ID)
 
     if channel:
-        await channel.send("✅ Bot GPU démarré avec succès !")
+        print("Vérification des prix...")
+        # Ici on ajoutera les recherches RTX
+        # await channel.send("Test surveillance RTX")
 
 client.run(TOKEN)
