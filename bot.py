b6bf618817a5aae0926e630a52f28a69d71004ee
christoprst-coder <christo.prst@gmail.com>
2026-06-20 13:18:20 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,25 +1,37 @@
 import discord
 import os
 from discord.ext import tasks
+from config import CHANNEL_ID, CHECK_INTERVAL
 
 TOKEN = os.getenv("DISCORD_TOKEN")
-CHANNEL_ID = 1517834960751427668
 
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
 
+already_sent = set()
+
 @client.event
 async def on_ready():
-    print(f"Connecté en tant que {client.user}")
-    check_prices.start()
+    print(f"✅ Connecté en tant que {client.user}")
 
-@tasks.loop(minutes=15)
+    channel = client.get_channel(CHANNEL_ID)
+    if channel:
+        await channel.send("🚀 Surveillance GPU démarrée")
+
+    if not check_prices.is_running():
+        check_prices.start()
+
+@tasks.loop(minutes=CHECK_INTERVAL)
 async def check_prices():
+    print("🔍 Vérification des prix...")
+
     channel = client.get_channel(CHANNEL_ID)
 
-    if channel:
-        print("Vérification des prix...")
-        # Ici on ajoutera les recherches RTX
-        # await channel.send("Test surveillance RTX")
+    if not channel:
+        return
+
+    # Ici nous ajouterons les scrapers plus tard
+    # Exemple :
+    # await channel.send("Test vérification")
 
 client.run(TOKEN)
